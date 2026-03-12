0706cba1d94258df70b0c8099083c74971623874
Hande Bengü <hande.bengu@chasacademy.se>
2026-03-12 19:00:23 +0100

    feat: add controllers, Swagger and configure Program.cs

---
+++ GamesAPI/Controllers/GenresController.cs
@@ -0,0 +1,105 @@
+﻿using GamesAPI.Data;
+using GamesAPI.DTOs;
+using GamesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamesAPI.Controllers
+{
+    [Route("api/genres")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GenresController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateGenreRequest([FromBody] CreateGenreRequest request)
+        {
+            var newGenre = new Genre
+            {
+                Name = request.Name
+            };
+
+            _context.Genres.Add(newGenre);
+            _context.SaveChanges();
+
+            var response = new GenreResponse
+            (
+                newGenre.Id,
+                newGenre.Name
+            );
+
+            return CreatedAtAction(nameof(GetGenreById), new { id = newGenre.Id }, response);
+        }
+
+        [HttpGet]
+        public IActionResult GetGenres()
+        {
+            var responseList = _context.Genres.Select(g => new GenreResponse
+            (
+                g.Id,
+                g.Name
+            )).ToList();
+            return Ok(responseList);
+        }
+
+        [HttpGet("{id}", Name = "GetGenreById")]
+        public IActionResult GetGenreById(int id)
+        {
+            var genre = _context.Genres.FirstOrDefault(g => g.Id == id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            var response = new GenreResponse
+            (
+                genre.Id,
+                genre.Name
+            );
+
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateGenre(int id, [FromBody] UpdateGenreRequest request)
+        {
+            var genre = _context.Genres.FirstOrDefault(g => g.Id == id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            genre.Name = request.Name ?? genre.Name;
+            _context.SaveChanges();
+
+            var response = new GenreResponse
+            (
+                genre.Id,
+                genre.Name
+            );
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteGenre(int id)
+        {
+            var genre = _context.Genres.FirstOrDefault(g => g.Id == id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            _context.Genres.Remove(genre);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ GamesAPI/Controllers/LibrariesController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using GamesAPI.Data;
+using GamesAPI.DTOs;
+using GamesAPI.Models;
+
+namespace GamesAPI.Controllers
+{
+    [Route("api/libraries")]
+    [ApiController]
+    public class LibrariesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LibrariesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+
+        public IActionResult CreateLibraryRequest([FromBody] CreateLibraryRequest request)
+        {
+            var newLibrary = new Library
+            {
+                Name = request.Name,
+                Description = request.Description
+            };
+
+            _context.Libraries.Add(newLibrary);
+            _context.SaveChanges();
+
+            var response = new LibraryResponse
+            (
+                newLibrary.Id,
+                newLibrary.Name,
+                newLibrary.Description,
+                newLibrary.CreatedAt,
+                newLibrary.UpdatedAt
+            );
+            return CreatedAtAction(nameof(GetLibraryById), new { id = newLibrary.Id }, response);
+        }
+
+        [HttpGet]
+        public IActionResult GetLibraries()
+        {
+            var responseList = _context.Libraries
+                .Select(l => new LibraryResponse(
+                    l.Id,
+                    l.Name,
+                    l.Description,
+                    l.CreatedAt,
+                    l.UpdatedAt
+                )).ToList();
+
+            return Ok(responseList);
+        }
+
+        [HttpGet("{id}", Name = "GetLibraryById")]
+        public IActionResult GetLibraryById(int id)
+        {
+            var library = _context.Libraries.FirstOrDefault(l => l.Id == id);
+
+            if (library == null)
+            {
+                return NotFound();
+            }
+
+            var response = new LibraryResponse
+            (
+                library.Id,
+                library.Name,
+                library.Description,
+                library.CreatedAt,
+                library.UpdatedAt
+            );
+
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult UpdateLibrary(int id, [FromBody] UpdateLibraryRequest request)
+        {
+            var library = _context.Libraries.FirstOrDefault(l => l.Id == id);
+
+            if (library == null)
+            {
+                return NotFound();
+            }
+
+            library.Name = request.Name ?? library.Name;
+            library.Description = request.Description ?? library.Description;
+            library.UpdatedAt = DateTime.UtcNow;
+
+            _context.SaveChanges();
+
+            var response = new LibraryResponse
+            (
+                library.Id,
+                library.Name,
+                library.Description,
+                library.CreatedAt,
+                library.UpdatedAt
+            );
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLibrary(int id)
+        {
+            var library = _context.Libraries.FirstOrDefault(l => l.Id == id);
+
+            if (library == null)
+            {
+                return NotFound();
+            }
+
+            _context.Libraries.Remove(library);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
